88887fe465af8af0fb95c2d3451e33ccf39482cb
innocent <ekweozorinnocent@gmail.com>
2024-05-05 22:30:01 +0100

    color mode

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { ChakraProvider } from "@chakra-ui/react";
 import "./index.css";
-import { extendTheme } from '@chakra-ui/react'
+import { extendTheme } from "@chakra-ui/react";
 
 const config = {
-  initialColorMode: 'light',
+  initialColorMode: "dark",
   useSystemColorMode: false,
-}
+};
+
+const theme = extendTheme({ config });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
