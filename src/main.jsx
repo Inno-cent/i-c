c65214adb74ca368e4119a8e9c3cc0e8cb63e14c
innocent <ekweozorinnocent@gmail.com>
2024-05-06 21:06:34 +0100

    color mode

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,34 @@ import App from "./App.jsx";
 import { ChakraProvider } from "@chakra-ui/react";
 import "./index.css";
 import { extendTheme } from "@chakra-ui/react";
+import {mode} from "@chakra-ui/theme-tools"
+
+// const styles = {
+//   global:(props) => ({
+//     body:{
+//       bg:mode('gray.100', "#000")(props),
+//       color:mode("grey.800", "whiteAlpha.900")(props)
+//     }
+//   })
+// }
 
 const config = {
   initialColorMode: "dark",
   useSystemColorMode: false,
 };
 
-const theme = extendTheme({ config });
+const theme = extendTheme({
+  config,
+  styles: {
+    global: (props) => ({
+      body: {
+        bg: mode('gray.100', "#000")(props),
+        color: mode("grey.800", "whiteAlpha.900")(props)
+      }
+    })
+  }
+});
+// const theme = extendTheme({ config.styles });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
