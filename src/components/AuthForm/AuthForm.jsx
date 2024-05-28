0dcd6ba644401ca40d6815888e758249aabdbf15
innocent <ekweozorinnocent@gmail.com>
2024-05-28 23:09:22 +0100

    auth update

---
+++ src/components/AuthForm/AuthForm.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Box, VStack } from "@chakra-ui/react";
+
+const AuthForm = () => {
+  return (
+    <div>
+      <Box border={"1px solid gray"} borderRadius={4} padding={5}>
+        <VStack spacing={4}></VStack>
+      </Box>
+    </div>
+  );
+};
+
+export default AuthForm;
