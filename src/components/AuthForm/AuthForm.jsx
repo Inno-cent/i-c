2ac0794a074943b6cd3eec9a1662fa5a365dc0f4
innocent <ekweozorinnocent@gmail.com>
2024-05-28 23:16:44 +0100

    auth update 2

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { Box, VStack } from "@chakra-ui/react";
+import { Box, VStack, Image } from "@chakra-ui/react";
 
 const AuthForm = () => {
   return (
     <div>
       <Box border={"1px solid gray"} borderRadius={4} padding={5}>
-        <VStack spacing={4}></VStack>
+        <VStack spacing={4}>
+            <Image src="/logo.png" h={24} cursor={"pointer"} alt="dd"/>
+        </VStack>
       </Box>
     </div>
   );
