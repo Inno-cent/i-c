4e382a5158d6dd47fc68f5ceba34d4cb5ea41510
innocent <ekweozorinnocent@gmail.com>
2024-05-29 19:43:18 +0100

    auth update 44

--- src/components/AuthForm/login.jsx
+++ src/components/AuthForm/login.jsx
@@ -26,7 +26,22 @@ const Login = () => {
 				value={inputs.password}
 				onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
 			/>
-			
+			{error && (
+				<Alert status='error' fontSize={13} p={2} borderRadius={4}>
+					<AlertIcon fontSize={12} />
+					{error.message}
+				</Alert>
+			)}
+			<Button
+				w={"full"}
+				colorScheme='blue'
+				size={"sm"}
+				fontSize={14}
+				isLoading={loading}
+				onClick={() => login(inputs)}
+			>
+				Log in
+			</Button>
 		</>
 	);
 };
